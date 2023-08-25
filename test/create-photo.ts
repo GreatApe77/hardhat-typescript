4db11795f3033485b225787227f013e9bfe908e8
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2023-08-25 01:02:45 -0300

    wrote a test

---
+++ test/create-photo.ts
@@ -0,0 +1,27 @@
+import "mocha";
+import {assert} from "chai";
+
+
+import hardhatEnv from "hardhat";
+import { Crud } from "../typechain-types";
+
+describe("Testing Create Photo Function in Crud", () => {
+	let crudContract:Crud;
+    const imageUrl = "https://some-image.jpeg";
+    const description = "Some Description";
+    before(async()=>{
+        let CrudFactory = await hardhatEnv.ethers.getContractFactory("Crud")
+        crudContract = await CrudFactory.deploy()
+    })
+	it("Should Post a foto using the following parameters {imageUrl:'https://some-image.jpeg', description:'Some Description'}", async () => {
+
+		const response = await crudContract.createPhoto(imageUrl, description);
+		assert(response, "Algo Errado");
+	});
+    it("Deve ler a foto postada e verificar o conteudo",async ()=>{
+        const foto = await crudContract.idToPhoto(1)
+
+        assert(foto.id===1n && foto.imageUrl === imageUrl && foto.description ===description,"Dados nao chegaram corretamente")
+
+    })
+});
